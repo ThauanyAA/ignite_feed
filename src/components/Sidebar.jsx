057da2fc235e448e314477791775362f41697259
thauanyAA <thauanyaraujo5@gmail.com>
2025-03-09 16:54:36 -0300

    Using Avatar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { PencilLine } from 'phosphor-react'
 import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
@@ -8,8 +9,7 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1522252234503-e356532cafd5?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
+        <Avatar
           src="https://avatars.githubusercontent.com/u/93295232?v=4"
         />
         <strong>Thauany Alves</strong>
